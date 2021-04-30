82f1700d9b5d4560185eeccca4069dc0528fe4fb
Serhii <Sergey.lazarenko@nixsolutions.com>
2021-04-30 17:12:24 +0300

    Task completed (master branch)

--- src/main/java/seleniumWebDriver/SearchPage.java
+++ src/main/java/seleniumWebDriver/SearchPage.java
@@ -7,10 +7,10 @@ import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 import org.openqa.selenium.support.PageFactory;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
+import java.util.*;
 
+import static java.lang.Double.parseDouble;
+import static java.lang.String.format;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 import static org.junit.Assert.assertEquals;
@@ -22,112 +22,73 @@ public class SearchPage {
     @FindBy(xpath = "//option[@value = 'price:desc']")
     private WebElement drpPriceHighFirst;
 
-    @FindBy(xpath = "//div[@class = 'right-block']//span[@class = 'price product-price']")
-    private List<WebElement> newPrice;
-
-    @FindBy(xpath = "//div[@class = 'right-block']//span[@class = 'old-price product-price']")
-    private List<WebElement> oldPrice;
-
-    @FindBy(xpath = "//div[@class = 'right-block']//span[@class = 'old-price product-price']")
-    private WebElement oldPriceTmp;
-
-    @FindBy(xpath = "//div[@class = 'right-block']//span[@class = 'price product-price' or @class = 'old-price product-price']")
-    private List<WebElement> price;
-
     @FindBy(xpath = "//div[@class = 'right-block']//a[@class = 'product-name']")
-    private WebElement itemName;
+    private List<WebElement> itemName;
 
     @FindBy(xpath = "//div[@class = 'right-block']//span[@class = 'price product-price']")
     private WebElement itemPrice;
 
     @FindBy(xpath = "//a[@title = 'Add to cart']")
-//            "//span[contains(text(),'Add to cart')]")
     private WebElement addToCart;
 
     @FindBy(xpath = "//a[@title = 'Proceed to checkout']")
     private WebElement moveToCart;
 
-    @FindBy(xpath = "//span[@id='total_product_price_5_19_0']")
-    private WebElement totalPriceCart;
-
-    @FindBy(xpath = "//td/p[@class = 'product-name']/a")
-    private WebElement cartItemName;
-
     @FindBy(xpath = "//div[@class='right-block']")
     private List<WebElement> priceBlock;
 
-    String saveItemName;
-    Double saveItemPrice;
+    @FindBy(xpath = "//tr[contains(@id,'product_')]")
+    private WebElement itemBlockCart;
 
     public SearchPage(WebDriver driver) {
         PageFactory.initElements(driver, this);
     }
 
-    public void isPageOpened() {
-        assertThat(search.getText(), containsString("SUMMER"));
+    public SearchPage verifySearchFilterIsDisplayed(String searchText) {
+        assertThat(search.getText(), equalToIgnoringCase(format("\"%s\"", searchText)));
+        return this;
     }
 
-    public void dropdownPriceHighestFirst() {
+    public SearchPage dropdownPriceHighestFirst() {
         drpPriceHighFirst.click();
+        return this;
     }
 
-    public void verifySort() {
+    public SearchPage verifySort() {
         List<Double> prices = new ArrayList<Double>();
         List<Double> sortedPrice = prices;
-//        for (WebElement element : newPrice) {
-//            convertToDoublePrice.add(Double.parseDouble(element.getText().replace("$", "")));
-//        }
         for (WebElement block : priceBlock) {
-            if (noSuchElement(block,".//span[@class = 'old-price product-price']")) {
-                prices.add(Double.parseDouble(block.findElement(By.xpath(".//span[@class = 'old-price product-price']")).getText().replace("$", "")));
-//                System.out.println(block.findElement(By.xpath(".//span[@class = 'old-price product-price']")).getText());
+            if (noSuchElement(block, ".//span[@class = 'old-price product-price']")) {
+                prices.add(formatStringToDouble(block.findElement(By.xpath(".//span[@class = 'old-price product-price']"))));
             } else {
-                prices.add(Double.parseDouble(block.findElement(By.xpath(".//span[@class = 'price product-price']")).getText().replace("$", "")));
+                prices.add(formatStringToDouble(block.findElement(By.xpath(".//span[@class = 'price product-price']"))));
             }
         }
-//        System.out.println(prices);
-
-//        List<Double> convertToDoublePriceTemp = priceBlock.stream().collect(Collectors.toList());
-//        System.out.println(convertToDoublePrice);
         sortedPrice.sort(Collections.reverseOrder());
-//                .stream()
-//                .sorted(Collections.reverseOrder())
-//                .collect(Collectors.toList());
-//        System.out.println(sortedPrice);
         assertEquals(prices, sortedPrice);
-
-//        List<Double> afterSortPrice = new ArrayList<Double>();
-//        for (WebElement element : price) {
-//            if (element.getText().contains("old")) {
-//                if (element.getText().contains("$")) {
-//                    afterSortPrice.add(Double.parseDouble(element.getText().replace("$", "")));
-//                }
-//            } else {
-//                if (newPrice.getText().contains("$")) {
-//                    afterSortPrice.add(Double.parseDouble(newPrice.getText().replace("$", "")));
-//                }
-//            }
-//        }
-//        System.out.println(afterSortPrice);
+        return this;
     }
 
-    public void saveItem() {
-        saveItemName = itemName.getText();
-        saveItemPrice = Double.parseDouble(itemPrice.getText().replace("$", ""));
+    public SearchPage saveItemNameAndPrice(Map<String, Double> mapPrice) {
+        if (mapPrice.containsKey(itemName)) {
+            mapPrice.put(itemName.stream().findFirst().get().getText(), mapPrice.get(itemName) + formatStringToDouble(itemPrice));
+        } else {
+            mapPrice.put(itemName.stream().findFirst().get().getText(), formatStringToDouble(itemPrice));
+        }
+        return this;
     }
 
-    public void addItemToCart() {
+    public SearchPage addItemToCart() {
         addToCart.click();
         moveToCart.click();
+        return this;
     }
 
-    public void comparePrice() {
-        double totalItemCartPrice = Double.parseDouble(totalPriceCart.getText().replace("$", ""));
-        assertThat(totalItemCartPrice, closeTo(saveItemPrice, 0.01));
-    }
-
-    public void compareName() {
-        assertThat(cartItemName.getText(), equalToIgnoringCase(saveItemName));
+    public SearchPage comparePriceFromCartTemp(Map<String, Double> mapPrice) {
+        mapPrice.entrySet().stream()
+                .filter(item -> item.getKey().equals(itemBlockCart.findElement(By.xpath(".//td/p[@class = 'product-name']/a")).getText()))
+                .forEach(item -> assertThat(item.getValue(), equalTo(formatStringToDouble(itemBlockCart.findElement(By.xpath(".//span[contains(@id,'total_product_price_')]"))))));
+        return this;
     }
 
     public boolean noSuchElement(WebElement webElement, String xpath) {
@@ -137,5 +98,9 @@ public class SearchPage {
         } catch (NoSuchElementException e) {
             return false;
         }
+    }
+
+    public Double formatStringToDouble(WebElement webElement) {
+        return parseDouble(webElement.getText().replace("$", ""));
     }
 }
--- src/main/java/seleniumWebDriver/SeleniumTest.java
+++ src/main/java/seleniumWebDriver/SeleniumTest.java
@@ -1,82 +1,57 @@
 package seleniumWebDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.junit.runners.Parameterized;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
 import org.testng.annotations.DataProvider;
+import org.testng.annotations.Test;
 
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.TimeUnit;
 
-//@RunWith(Parameterized.class)
 public class SeleniumTest {
     WebDriver driver;
-//    private String firstSearch;
-//    private String secondSearch;
-//    private String thirdSearch;
 
-//    public SeleniumTest(String firstSearch, String secondSearch, String thirdSearch) {
-//        super();
-//        this.firstSearch = firstSearch;
-//        this.secondSearch = secondSearch;
-//        this.thirdSearch = thirdSearch;
-//    }
-//
-//    @Parameterized.Parameters
-//    public static searchItems() {
-//        return;
-//    }
-
-    public class DataProviderClass
-    {
-        @DataProvider(name = «data-provider»)
-        public static Object[][] dataProviderMethod()
-        {
-            return new Object[][] { { «data one» }, { «data two» } };
-        }
-    }
-
-    @Before
+    @BeforeTest
     public void setup() {
         WebDriverManager.chromedriver().setup();
+    }
+
+    @DataProvider(name = "data-provider")
+    public Object[][] dataProviderMethod() {
+        return new Object[][]{{"summer"}, {"dress"}, {"t-shirt"}};
+    }
+
+    @Test(dataProvider = "data-provider")
+    public void applySearchSummer(String data) {
         driver = new ChromeDriver();
         driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
-    }
-
-    @Test
-    public void applySearchSummer() {
+        Map<String, Double> priceMap = new HashMap<String, Double>();
+        //Step1
         HomePage home = new HomePage(driver);
-        home.setSearch("summer");
+        //Step2
+        home.setSearch(data);
+        //Step3
         SearchPage searchPage = new SearchPage(driver);
-        searchPage.isPageOpened();
-        searchPage.dropdownPriceHighestFirst();
-        searchPage.verifySort();
-        searchPage.saveItem();
-        searchPage.addItemToCart();
-        searchPage.comparePrice();
-        searchPage.compareName();
-    }
-
-    @Test
-    public void applySearchDress() {
-        HomePage home = new HomePage(driver);
-        home.setSearch("dress");
-        SearchPage searchPage = new SearchPage(driver);
-        searchPage.isPageOpened();
-        searchPage.dropdownPriceHighestFirst();
-        searchPage.verifySort();
-        searchPage.saveItem();
-        searchPage.addItemToCart();
-        searchPage.comparePrice();
-        searchPage.compareName();
-    }
-
-    @After
-    public void close() {
+        searchPage.verifySearchFilterIsDisplayed(data)
+                //Step4
+                .dropdownPriceHighestFirst()
+                //Step5
+                .verifySort()
+                //Step6
+                .saveItemNameAndPrice(priceMap)
+                //Step7
+                .addItemToCart()
+                //Step8
+                .comparePriceFromCartTemp(priceMap);
         driver.close();
+    }
+
+    @AfterTest
+    public void close() {
+        driver.quit();
     }
 }
